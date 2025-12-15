0e774b751e5ddac0c864f4cdd2c19aa668489fb0
Iliasse <elabouyi.iliasse@gmail.com>
2025-12-15 17:27:42 +0100

    feat: implementing a servlet with configs

---
+++ src/example/HelloServlet.java
@@ -0,0 +1,16 @@
+package example;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/plain; charset=UTF-8");
+        resp.getWriter().println("Hello, world!");
+    }
+}
